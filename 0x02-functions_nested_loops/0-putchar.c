e9026b676e4839e63efdb07f6b9fc5589d848a46
KingTree88 <ltmotaung88@gmail.com>
2023-02-21 11:16:48 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "putchar.h"
+/**
+ * main - Entry point
+ *
+ * Return: Alway 0 (Sucess)
+ */
+
+int main(void)
+{
+	char *h = "putchar\n";
+
+	while (*h)
+	{
+		_putchar(*h);
+		h++;
+	}
+	return (0);
+}
